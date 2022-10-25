5a36868581cce7aab6fbcd3e27d94f0a6223adcc
tweltysnow <116674224+tweltysnow@users.noreply.github.com>
2022-10-25 14:43:39 -0400

    Update streamlit_app.py
    second header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('🍜 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacaco Toast')
+
+streamlit.header('🍌🍓 Build Your Own Fruit Smoothie 🥝🍇')
